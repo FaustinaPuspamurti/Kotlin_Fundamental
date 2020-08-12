5538b25b99a585fcdd4d160d6e010a024c136f38
Faustina Puspamurti <55085124+FaustinaPuspamurti@users.noreply.github.com>
2020-08-12 12:25:26 +0700

    Update Boolean.kt

--- Boolean.kt
+++ Boolean.kt
@@ -1,15 +1,15 @@
 fun main (){
     val kkm =  80
-    val nilai = 100
+    val nilai = 40
     val nilaiuh = nilai > kkm
 
     if (nilai >90) {
         print("A")
-    } else if (nilai >80 ){
+    } else if (nilai >80 && nilai<=90 ){
         print("B")
-    } else if (nilai >70 ){
+    } else if (nilai >70 && nilai<=80){
         print("C")
-    } else if (nilai >60 ) {
+    } else if (nilai >=60 && nilai<=70 ) {
         print("D")
     } else {
         print("E")
